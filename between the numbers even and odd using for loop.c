586a68d63a48dd4d41dfe5c86045f24337f0b9e8
divya-bikkina <96426510+divya-bikkina@users.noreply.github.com>
2021-12-20 19:31:29 +0530

    Create between the numbers even and odd using for loop.c

---
+++ between the numbers even and odd using for loop.c
@@ -0,0 +1,35 @@
+nclude <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main() 
+{
+    int a=0,b=0,n=0;
+    char*arr[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+    scanf("%d\n%d", &a, &b);
+    for(n=a;n<=b;n++)
+    {
+        if(n>9)
+        {
+            if(n%2==0)
+            {
+            printf("even\n");
+            }
+            else  
+            {
+            printf("odd\n");
+            }
+        }
+            else
+            {
+            puts(arr[n]);
+            }
+        
+    }
+    
+
+
+    return 0;
+}
